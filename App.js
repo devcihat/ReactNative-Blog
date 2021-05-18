807e1db3a005f64a04ed36b8a43d38615a0bb4de
devcihat <cihattsungu@outlook.com>
2021-05-18 09:23:10 +0300

    add useReducer

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import { createAppContainer } from "react-navigation"
-import { createStackNavigator } from "react-navigation-stack"
-import IndexScreen from "./src/screens/IndexScreen"
-import { BlogProvider } from "./src/context/BlogContext"
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import IndexScreen from "./src/screens/IndexScreen";
+import { Provider } from "./src/context/BlogContext";
 
-const navigator = createStackNavigator({
-  Index:IndexScreen
-}, {
-  initialRouteName:"Index",
-  defaultNavigationOptions: {
-    title:'Blogs'
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+  },
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blogs",
+    },
   }
-})
+);
 
-const App =  createAppContainer(navigator)
+const App = createAppContainer(navigator);
 
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
-  )
-}
+    </Provider>
+  );
+};
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList,Button } from "react-native";
-import BlogContext from "../context/BlogContext";
+import { Context } from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const { data, addBlogPost } = useContext(BlogContext);
+  const { state, addBlogPost } = useContext(Context);
   //console.log(addBlogPost)
   //console.log("bbb", addBlogPost)
   //console.log("aaa",blogPosts)
@@ -13,7 +13,7 @@ const IndexScreen = () => {
       <Text>Index Screen</Text>
       <Button onPress={addBlogPost} title="Add Post"></Button>
       <FlatList
-        data={data}
+        data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>;
